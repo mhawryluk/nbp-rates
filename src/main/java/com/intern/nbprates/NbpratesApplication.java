ad1118dc483c575df1fd8dd5bc15d55fa98be39c
mhawryluk <marcinhawrylukxx@gmail.com>
2022-02-22 18:14:11 +0100

    Generate project template

---
+++ src/main/java/com/intern/nbprates/NbpratesApplication.java
@@ -0,0 +1,13 @@
+package com.intern.nbprates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbpratesApplication.class, args);
+	}
+
+}
